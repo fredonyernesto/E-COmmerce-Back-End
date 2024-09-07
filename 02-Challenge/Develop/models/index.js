062f4a6cca7e700874ec59d8a7e1427661e0e4df
SHMERNIE <fredony.ernesto@gmail.com>
2024-09-06 19:38:35 -0700

    finished setting up the models, super confident it correct without testing it .. lol

--- 02-Challenge/Develop/models/index.js
+++ 02-Challenge/Develop/models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+foreingKey: 'product_id',
+onDelete:'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+through: {
+  model: ProductTag,
+  unqiue: false
+}
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+through: {
+  model: ProductTag,
+  unqiue: false
+}
+})
 
 module.exports = {
   Product,
--- 02-Challenge/Develop/routes/api/category-routes.js
+++ 02-Challenge/Develop/routes/api/category-routes.js
@@ -6,7 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    
+  }
 });
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
